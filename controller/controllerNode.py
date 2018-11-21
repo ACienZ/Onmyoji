a6aa9b7a7ae0e237cf40dd07c886c4b103d01906
Administrator ⊙ω⊙ <986498200@qq.com>
2018-11-21 22:44:48 +0800

    change controller

--- controller/controllerNode.py
+++ controller/controllerNode.py
@@ -1,3 +1,24 @@
 #ControllerNode
 #user to control mouse and keyboard
 
+#2018.11.21
+#create class
+
+from pynput.mouse import Button,Controller
+
+class controllerNode:
+
+    @classmethod
+    def mouseteleport(cls,position1,position2):
+        mouse=Controller()
+        mouse.move(position1,position2)
+
+    @classmethod
+    def mouseMove(cls,position1,position2):
+        mouse=Controller()
+        positA,positB=mouse.position
+        ###########
+        print(positA,positB)
+        for i in range(position1):
+            for j in range(position2):
+
